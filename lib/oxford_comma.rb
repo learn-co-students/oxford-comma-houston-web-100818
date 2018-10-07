2420b8586b5b390780a4d6d310c0282d600f631e
Alexander Carlin <ahcarlin@gmail.com>
2018-10-07 02:39:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size <= 1 
+    return array.join
+  end
+  if array.size == 2 
+    return array.join(" and ")
+  end
+  
+  ca = array.insert(-2, "and")
+  ca = array.join(", ")
+  ca[ca.rindex(',')] = ''
+  return ca
 end
